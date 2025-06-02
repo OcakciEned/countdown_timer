4c33da09d825c5b828e2cd983970884d5918d402
enesocakcı <enesocakci343434@gmail.com>
2025-06-02 19:26:31 +0300

    güncelleme3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,14 +23,11 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.countdown_timer"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdkVersion 19
-        targetSdkVersion 33
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdkVersion(23)
+        targetSdkVersion( 33 )
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
